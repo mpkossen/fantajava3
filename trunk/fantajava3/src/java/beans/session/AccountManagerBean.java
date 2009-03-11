c5045da3d0aeea1398dd31d28ca579a57c805ba4
maarten.kossen <maarten.kossen@455e4ee2-024e-11de-8c4e-4bbb75a408b7>
2009-03-11 08:59:18 +0000

    Created empty beans and packages.

---
+++ trunk/fantajava3/src/java/beans/session/AccountManagerBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans.session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author mistermartin75
+ */
+@Stateless
+public class AccountManagerBean implements AccountManagerLocal {
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method" or "Web Service > Add Operation")
+ 
+}
