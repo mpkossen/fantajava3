c27c9a4792c7d68b271887445fa6decd700419bb
maarten.kossen <maarten.kossen@455e4ee2-024e-11de-8c4e-4bbb75a408b7>
2009-03-16 10:16:44 +0000

    Importing IF project.

---
+++ fantajava3-IF/src/remote/AccountManagerIF.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package remote;
+
+/**
+ *
+ * @author mistermartin75
+ */
+public interface AccountManagerIF {
+
+}
