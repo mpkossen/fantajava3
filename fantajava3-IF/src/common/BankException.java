a402af130f189c4d2df153512f4f4d961ce31e42
maarten.kossen <maarten.kossen@455e4ee2-024e-11de-8c4e-4bbb75a408b7>
2009-03-16 11:05:03 +0000

    Modified package names.

--- fantajava3-IF/src/common/BankException.java
+++ fantajava3-IF/src/common/BankException.java
@@ -1,4 +1,4 @@
-package bank;
+package common;
 
 public class BankException
 extends Exception
--- fantajava3-IF/src/common/MD5.java
+++ fantajava3-IF/src/common/MD5.java
@@ -1,4 +1,4 @@
-package bank.util;
+package common;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
