a90329c2ad0fa2ffab029643a82a64375fd18410
maarten.kossen <maarten.kossen@455e4ee2-024e-11de-8c4e-4bbb75a408b7>
2009-04-07 11:17:07 +0000

    All this crap was necessary to make this work.

---
+++ fantajava3-JSF/src/java/beans/BankAccessBean.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+import javax.ejb.EJB;
+import remote.*;
+
+/**
+ *
+ * @author mistermartin75
+ */
+public class BankAccessBean extends CommonBean {
+
+	/*private static final long serialVersionUID = 1L;
+	private static int ID = 0;
+
+	private static int getId() {
+		return ID++;
+	}
+	private int id = getId();
+
+	@EJB(mappedName = "jnp://145.89.207.42:1099/AccountManager/remote")
+	private AccountManagerIF accountManager = null;
+
+	@EJB(mappedName = "jnp://145.89.207.42:1099/AccountOffice/remote")
+	private AccountOfficeIF accountOffice = null;*/
+	
+}
