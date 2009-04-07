58b5bc03d9c0fcd600f97d49bbad6cbe0a0b872b
wouter.oet <wouter.oet@455e4ee2-024e-11de-8c4e-4bbb75a408b7>
2009-04-07 13:23:48 +0000

    Oeps vergeten :P

--- fantajava3-JSF/src/java/beans/manager/NewAccountBean.java
+++ fantajava3-JSF/src/java/beans/manager/NewAccountBean.java
@@ -5,48 +5,48 @@
 package beans.manager;
 
 import beans.CommonBean;
+import beans.Connector;
 import common.BankException;
 import jaas.MyPrincipal;
+import java.lang.reflect.Method;
 import javax.faces.context.FacesContext;
 import remote.AccountManagerIF;
 
-
-public class NewAccountBean extends CommonBean {
-
-	private AccountManagerIF accountManager;
+public class NewAccountBean extends CommonBean
+{
 	private String naam,  pincode;
 	private Double limiet;
 
-	public NewAccountBean() {
+	public NewAccountBean()
+	{
 		System.out.println("NewAccountBean");
-                limiet = 10000.00;
+		limiet = 10000.00;
 	}
 
-	protected AccountManagerIF getAccountManager() {
-		System.out.println("getAccountManager()");
-		if (accountManager == null) {
-			MyPrincipal mp =
-					(MyPrincipal) FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
-			return accountManager = mp.getAccountManager();
-		} else {
-			return accountManager;
-		}
+	protected AccountManagerIF getAccountManager()
+	{
+		return Connector.getAccountManager();
 	}
 
 	/**
 	 * @return the limiet
 	 */
-	public String getLimiet() {
-		return limiet + "";
+	public String getLimiet()
+	{
+		return limiet+"";
 	}
 
 	/**
 	 * @param limiet the limiet to set
 	 */
-	public void setLimiet(String limiet) {
-		try {
+	public void setLimiet(String limiet)
+	{
+		try
+		{
 			this.limiet = Double.parseDouble(limiet);
-		} catch (NumberFormatException nfe) {
+		}
+		catch (NumberFormatException nfe)
+		{
 			setMessage("Limiet moet een nummer zijn");
 		}
 	}
@@ -54,32 +54,51 @@ public class NewAccountBean extends CommonBean {
 	/**
 	 * @return the naam
 	 */
-	public String getNaam() {
+	public String getNaam()
+	{
 		return naam;
 	}
 
 	/**
 	 * @param naam the naam to set
 	 */
-	public void setNaam(String naam) {
+	public void setNaam(String naam)
+	{
+		System.out.println("Set naam: "+naam);
 		this.naam = naam;
 	}
 
-	public String getPincode() {
+	public String getPincode()
+	{
 		return pincode;
 	}
 
-	public void setPincode(String pincode) {
+	public void setPincode(String pincode)
+	{
+		System.out.println("Set pincode: "+pincode);
 		this.pincode = pincode;
 	}
 
-	public void opslaan() {
-		try {
-			setMessage("Account is succesvol gemaakt, en " + getAccountManager().newAccount(limiet, naam, pincode));
-                        setNaam("");
-                        setPincode("");
-		} catch (BankException ex) {
+	public void opslaan()
+	{
+		AccountManagerIF a;
+		try
+		{
+			System.out.println("opslaan");
+			a = getAccountManager();
+			System.out.println(a.getClass());
+			for(Method m : a.getClass().getDeclaredMethods()){
+				System.out.println(m.getName());
+			}
+			setMessage("Account is succesvol gemaakt, en "+getAccountManager().newAccount(limiet, naam, pincode));
+			System.out.println("After message");
+			setNaam("");
+			setPincode("");
+		}
+		catch (BankException ex)
+		{
 			setMessage("Nieuw account aanmaken is mislukt");
 		}
 	}
+
 }
